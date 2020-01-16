1a1b2c6a76388467921bc28ff507d38269395790
Christopher Fenner <Christopher.Fenner@sap.com>
2020-01-16 21:47:13 +0100

    fix: correct install formula

--- Formula/mbt.rb
+++ Formula/mbt.rb
@@ -1,6 +1,9 @@
 class Mbt < Formula
-  homepage 'https://github.com/SAP/cloud-mta-build-tool'
-  version '1.0.7'
+  desc ""
+  homepage "https://github.com/SAP/cloud-mta-build-tool"
+  #version '1.0.7'
+  #url "https://github.com/SAP/cloud-mta-build-tool/releases/download/v1.0.7/cloud-mta-build-tool_1.0.7_Darwin_amd64.tar.gz"
+  #sha256 "aa2d51a0180c6ef00fe72aec644775d1b65cae9c73f74355b86c1f05f36b11d3"
 
   if OS.mac?
     url 'https://github.com/SAP/cloud-mta-build-tool/releases/download/v1.0.7/cloud-mta-build-tool_1.0.7_Darwin_amd64.tar.gz'
@@ -10,15 +13,13 @@ class Mbt < Formula
     sha256 '6feb60a67973134afeb3b320e9c06d925ea076972def0a8aa6d201f4e7d24847'
   end
 
-  #depends_on :arch => :x86_64
-
   def install
-    bin.install 'mbt'
-    doc.install 'LICENSE'
-    doc.install 'README.md'
+    bin.install "mbt"
+    doc.install "LICENSE"
+    doc.install "README.md"
   end
 
   test do
-    system "#{bin}/mbt"
+    system "#{bin}/mbt --help"
   end
 end
