21ba4fc10d3af4c8f0cfef9c90992c76db38d441
Christopher Fenner <26137398+CCFenner@users.noreply.github.com>
2020-01-20 13:38:48 +0100

    feat: update mbt to v1.0.8

--- Formula/mbt.rb
+++ Formula/mbt.rb
@@ -1,17 +1,14 @@
 class Mbt < Formula
   desc "Multi-Target Application (MTA) build tool for Cloud Applications"
   homepage "https://github.com/SAP/cloud-mta-build-tool"
-  #url "https://github.com/SAP/cloud-mta-build-tool/releases/download/v1.0.7/cloud-mta-build-tool_1.0.7_Darwin_amd64.tar.gz"
-  #sha256 "aa2d51a0180c6ef00fe72aec644775d1b65cae9c73f74355b86c1f05f36b11d3"
-
-  version '1.0.7'
+  version '1.0.8'
 
   if OS.mac?
     url "https://github.com/SAP/cloud-mta-build-tool/releases/download/v#{version}/cloud-mta-build-tool_#{version}_Darwin_amd64.tar.gz"
-    sha256 'aa2d51a0180c6ef00fe72aec644775d1b65cae9c73f74355b86c1f05f36b11d3'
+    sha256 'c802b006546e9821d092493b5e0e7040469d852e41da9096d4afea9baddeba22'
   elsif OS.linux?
     url "https://github.com/SAP/cloud-mta-build-tool/releases/download/v#{version}/cloud-mta-build-tool_#{version}_Linux_amd64.tar.gz"
-    sha256 '6feb60a67973134afeb3b320e9c06d925ea076972def0a8aa6d201f4e7d24847'
+    sha256 'b852d8884bd21b1b505b95b6d28ecbd3668e136d07f852a7efd76c9ab371756f'
   end
 
   def install
